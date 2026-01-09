04ae16bdfe9827f52f2aa76c96e8f8f921c7dc97
AvhiMaz <avhimazumder5@outlook.com>
2026-01-09 14:19:41 +0530

    CURSOR POINTER
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -26,7 +26,7 @@ export function ThemeToggle() {
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="w-9 h-9"
+      className="w-9 h-9 cursor-pointer"
     >
       <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
